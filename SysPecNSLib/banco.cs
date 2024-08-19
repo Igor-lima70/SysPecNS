a53839096529f1c8be6794617b7a814b29a2a886
IGOR TAVARES DE LIMA <igorlima7080@gmail.com>
2024-08-19 11:52:54 -0300

    modifiquei o banco.cs

--- SysPecNSLib/banco.cs
+++ SysPecNSLib/banco.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace SysPecNSLib
 {
-    public class banco
+    public static class Banco //Classe de conexão de banco de dados 
     {
+        /// <summary>
+        /// Método abrir da classe Banco é utilizado para representar comandos SQL
+        /// no servidor, cujos os dados foram informados na string de conexão (strconn)
+        /// </summary>
+        /// <returns>Entrega um objeto no SQL, que serão executados na conexão informada</returns>
+        public static MySqlCommand Abrir() //método de conexão
+        {
+            // dados de conexão
+            string strconn = @"server=127.0.0.1;database=syspecdb;user=root;password=";
+            MySqlConnection cn = new MySqlConnection(strconn);
+            MySqlCommand cmd = new();
+            try // Tratamento de excessão
+            {
+                cn.Open();
+                cmd.Connection = cn;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return cmd;
+        }
     }
 }
